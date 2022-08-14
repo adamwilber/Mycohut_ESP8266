49c34e40be6755582353ee253cb1ababcadc7305
adamwilber <111248591+adamwilber@users.noreply.github.com>
2022-08-14 15:35:07 -0500

    Update main.py
    Broke the logic into separate functions and call them within the main loop.  Added global variable "clock_cycle" used by the sleep command ad the end of the main loop for easier updating of the delay between loop cycles.

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ import ntptime
 import utime
 
 
+### sets the delay between cycles of reading from the sensor and cycling hardware
+clock_cycle = 10
+
 ### Set timezone offset
 time_offset = -5
 time_offset = time_offset * 60 * 60
@@ -60,59 +63,9 @@ oled_width = 128
 oled_height = 64
 oled = ssd1306.SSD1306_I2C(oled_width, oled_height, i2c)
 
-### Main loop
-while 1 == 1:
-
-  # Get current time from internet
-  try:
-    ntptime.settime()
-  except Exception as e:
-    print('error setting time')
-
-  # Unpack time into variables
-  year, month, day, hour, minute, second, ms, dayinyear = utime.gmtime(time.time() + time_offset)
-  print(hour,minute)  ### for testing, can remove
-
-
-
-  # now = str(hour) + str(minute)
-
-
-  ### Take measurements from DHT22 sensor
-  sensor.measure()
-  humidity = sensor.humidity()
-  temp = sensor.temperature()
-
-  ### Convert temp to F
-  temp = temp * 1.8 + 32
-
-  ### Clear screen then display temp and humidity to OLED screen
-  ### Turn on and off fog relay based on humidity
-  if humidity >= set_high_hum:
-    fog_relay.value(1)  # Turn off fog relay
-    fan_relay.value(1)  # Turn off fan relay
-  if humidity <= set_low_hum:
-    fog_relay.value(0) # Turn on fog relay
-    fan_relay.value(0) # Turn on fan relay
-
-  ### Turn lights on or off based on time set above
-  if (hour >= lights_on_hour) and (hour < lights_off_hour):
-    led_relay.value(0) # Turn on LED relay
-  else:
-    led_relay.value(1) # Turn off LED relay
-
-  ### Turn UV sterilizer on or off based on time set above
-  if (hour >= uv_on_hour) and (hour < uv_off_hour):
-    uv_relay.value(0) # Turn on LED relay
-  else:
-    uv_relay.value(1) # Turn off LED relay
-
-  ### Formatting correction for display
-  if len(str(minute)) == 1:
-    minute = "0" + str(minute)
-  nowseconds = str(hour) + ":" + str(minute) + ":" + str(second)
-
-  ### Display info on OLED screen
+def displayOLED(tmp, hum, addr, time):
+  ## Display info on OLED screen
+  global oled
   oled.fill(0)
   oled.show()
   oled.text('MycoHut', 0, 0, 16)
@@ -121,5 +74,74 @@ while 1 == 1:
   oled.text('IP: {}'.format(IP), 0, 40)
   oled.text('Time: {}'.format(nowseconds), 0, 50)
   oled.show()
-  sleep(10)
+
+def readSensor():
+  ## Read info from DHT22 sensor
+  global sensor
+  global humidity
+  global temp
+  sensor.measure()
+  humidity = sensor.humidity()
+  temp = sensor.temperature() * 1.8 +32
+  
+def cycleFogger(hum):
+  ### Turn on and off fog relay based on humidity
+  global fog_relay
+  global fan_relay
+  global set_high_hum
+  global set_low_hum
+  if hum >= set_high_hum:
+    fog_relay.value(1)  # Turn off fog relay
+    fan_relay.value(1)  # Turn off fan relay
+  if hum <= set_low_hum:
+    fog_relay.value(0) # Turn on fog relay
+    fan_relay.value(0) # Turn on fan relay
+
+def cycleLighting(hour):
+  global lights_on_hour
+  global lights_off_hour
+  global led_relay
+  if (hour >= lights_on_hour) and (hour < lights_off_hour):
+    led_relay.value(0) # Turn on LED relay
+  else:
+    led_relay.value(1) # Turn off LED relay
+
+def cycleSterilizer(hour):
+  ### Turn UV sterilizer on or off based on time set above
+  global uv_on_hour
+  global uv_off_hour
+  global uv_relay
+  ### Turn UV sterilizer on or off based on time set above
+  if (hour >= uv_on_hour) and (hour < uv_off_hour):
+    uv_relay.value(0) # Turn on LED relay
+  else:
+    uv_relay.value(1) # Turn off LED relay
+
+def getTime():
+  global time_offset
+  global nowseconds
+  # Get current time from internet
+  try:
+    ntptime.settime()
+  except Exception as e:
+    print('error setting time')
+  # Unpack time into variables
+  year, month, day, hour, minute, second, ms, dayinyear = utime.gmtime(time.time() + time_offset)
+    ### Formatting correction for display
+  if len(str(minute)) == 1:
+    minute = "0" + str(minute)
+  nowseconds = str(hour) + ":" + str(minute) + ":" + str(second)
+  return hour
+
+### Main loop
+while 1 == 1:
+
+  currentHour = getTime()
+  displayOLED(temp, humidity, IP, nowseconds)
+  readSensor()
+  cycleFogger(humidity)
+  cycleLighting(currentHour)
+  cycleSterilizer(currentHour)
+  getTime()
+  sleep(clock_cycle)
 
